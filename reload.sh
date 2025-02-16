83792eb5ba72009a45c2752b5af2da0bc43ffaa4
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2025-02-16 13:44:38 +0100

    kleo

--- reload.sh
+++ reload.sh
@@ -1,6 +1,9 @@
 cd /root/skrat-org
 git pull origin main
 go build
-cd web
-npx vite build
+if [ "$#" -e 2 ]; then
+  cd web
+  npx vite build
+fi
 systemctl restart skrat-org
+journalctl -u skrat-org
