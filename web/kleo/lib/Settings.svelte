1aae70bd2ccc4c97bc7fb59d29021bdfa90c8d18
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2025-03-04 19:57:51 +0100

    kleo

--- web/kleo/lib/Settings.svelte
+++ web/kleo/lib/Settings.svelte
@@ -16,23 +16,27 @@
   }
 
   async function setupNotifs() {
-    await navigator.serviceWorker.register("/kleo/service-worker.js");
-    let perm = await Notification.requestPermission();
-    console.log(perm);
-    if (perm !== "granted") { return }
-    let reg = await navigator.serviceWorker.ready;
-    const subscription = await reg.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: "BGl8lG0dFZxVzpEwgnPQlHaqDuaBojbFJHJzh2CMYi8mZshivG7RRkGDLKAC6E23E6ELtp3ikBXuepRJBMRlbwc",
-    });
-    let resp = await pb.send("/api/kleo/setupnotifs", {
-      method: "POST",
-      body: {
-        vapid: JSON.stringify(subscription.toJSON())
-      }
-    });
-    console.log(resp);
-    alert("alles OK, přihlásili jsme tě na notifkace");
+    try {
+      await navigator.serviceWorker.register("/kleo/service-worker.js");
+      let perm = await Notification.requestPermission();
+      console.log(perm);
+      if (perm !== "granted") { return }
+      let reg = await navigator.serviceWorker.ready;
+      const subscription = await reg.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: "BGl8lG0dFZxVzpEwgnPQlHaqDuaBojbFJHJzh2CMYi8mZshivG7RRkGDLKAC6E23E6ELtp3ikBXuepRJBMRlbwc",
+      });
+      let resp = await pb.send("/api/kleo/setupnotifs", {
+        method: "POST",
+        body: {
+          vapid: JSON.stringify(subscription.toJSON())
+        }
+      });
+      console.log(resp);
+      alert("alles OK, přihlásili jsme tě na notifkace");
+    } catch (e) {
+      alert(e);
+    }
   }
 
   async function vapidTest() {
