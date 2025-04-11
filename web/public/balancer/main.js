336b86ba68b730be7f8bb7da143625a90cfc31c3
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2025-04-11 19:52:46 +0200

    balancer

---
+++ web/public/balancer/main.js
@@ -0,0 +1,22 @@
+async function getPerm() {
+  let device = await navigator.bluetooth.requestDevice({acceptAllDevices: true});
+  await device.gatt.connect();
+}
+// async function getPerm() {
+//   let device = await navigator.usb.requestDevice({filters: [{vendorId: 0x0d28}]});
+//   console.log(device);
+//   await device.open();
+//   await device.selectConfiguration(1);
+//   await device.claimInterface(2);
+//   device.controlTransferOut({
+//       requestType: 'class',
+//       recipient: 'interface',
+//       request: 0x22,
+//       value: 0x01,
+//       index: 0x02});
+//   let decoder = new TextDecoder();
+//   while (true) {
+//     let res = await device.tranferIn(5, 5);
+//     console.log(decoder.decode(res.data));
+//   }
+// }
