2b32de9a22fac70540ed85c6e0455b3ebdec2cce
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2025-04-11 20:28:50 +0200

    balancer

--- web/public/balancer/main.js
+++ web/public/balancer/main.js
@@ -7,7 +7,7 @@ async function getPerm() {
     characteristic.addEventListener('characteristicvaluechanged', function (evt) {
       document.querySelector("h1").innerText = typeof evt.target.value;
     });
-    document.querySelector("h1").innerText = JSON.stringify(characteristic.properties);
+    document.querySelector("h1").innerText = JSON.stringify(characteristic.properties.read);
   } catch (e) {
     alert(e);
   }
