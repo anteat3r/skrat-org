e256b0648f4c6241df2cc2ba85737248f47bf828
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2025-04-11 20:33:57 +0200

    balancer

--- web/public/balancer/main.js
+++ web/public/balancer/main.js
@@ -3,9 +3,11 @@ async function getPerm() {
   let device = await navigator.bluetooth.requestDevice({filters: [{namePrefix: "BBC"}], optionalServices: ["6e400001-b5a3-f393-e0a9-e50e24dcca9e"]});
     await device.gatt.connect();
     let service = await device.gatt.getPrimaryService("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
-    let characteristic = await service.getCharacteristic("6e400002-b5a3-f393-e0a9-e50e24dcca9e");
-    let idk = await characteristic.startNotifications();
-    document.querySelector("h1").innerText = JSON.stringify(idk);
+    let characteristic = await service.getCharacteristic("6e400003-b5a3-f393-e0a9-e50e24dcca9e");
+    characteristic.addEventListener('characteristicvaluechanged', function (evt) {
+      document.querySelector("h1").innerText = typeof evt.target.value;
+    });
+    document.querySelector("h1").innerText = await characteristic.readValue();
   } catch (e) {
     alert(e);
   }
