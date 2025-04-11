f58b5d54a7af5f2dbfb592970248b4c0073c001b
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2025-04-11 20:38:52 +0200

    balancer

--- web/public/balancer/main.js
+++ web/public/balancer/main.js
@@ -4,8 +4,8 @@ async function getPerm() {
     await device.gatt.connect();
     let service = await device.gatt.getPrimaryService("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
     let characteristic = await service.getCharacteristic("6e400003-b5a3-f393-e0a9-e50e24dcca9e");
-    // let idk = await characteristic.startNotifications();
-    document.querySelector("h1").innerText = JSON.stringify(idk);
+    let idk = await characteristic.startNotifications();
+    document.querySelector("h1").innerText = await characteristic.readValue();
   } catch (e) {
     alert(e);
   }
